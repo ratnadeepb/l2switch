883c8b8afb640ce7b9f4accc71b5522277ef1f77
Ratnadeep Bhattacharya <bhattacharya.ratnadeep@gmail.com>
2020-11-17 04:05:54 +0000

    adding the five tuple struct

--- sw-engine/engine/src/net/mod.rs
+++ sw-engine/engine/src/net/mod.rs
@@ -22,8 +22,34 @@ mod cidr;
 mod ether;
 mod ipv4;
 mod mac;
+mod routing_table;
 
 pub use self::cidr::{Cidr, CidrError, Ipv4Cidr, Ipv6Cidr};
 pub use self::ether::EtherHdr;
 pub use self::ipv4::Ipv4Hdr;
 pub use self::mac::{MacAddr, MacParseError};
+pub use self::routing_table::RoutingTable;
+
+use std::net::Ipv4Addr;
+
+pub struct FiveTuple {
+	src_mac: MacAddr,
+	dst_mac: MacAddr,
+	src_ip: Ipv4Addr,
+	dst_ip: Ipv4Addr,
+	proto: u8,
+}
+
+impl FiveTuple {
+	pub fn new(ipv4_hdr: Ipv4Hdr, ether_hdr: EtherHdr) -> Self {
+		let ehdr = ether_hdr.get();
+		let ihdr = ipv4_hdr.get();
+		Self {
+			src_mac: ehdr.s_addr.addr_bytes.into(),
+			dst_mac: ehdr.d_addr.addr_bytes.into(),
+			src_ip: ihdr.src_addr.into(),
+			dst_ip: ihdr.dst_addr.into(),
+			proto: ihdr.next_proto_id,
+		}
+	}
+}

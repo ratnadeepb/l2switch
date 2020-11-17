d61722de2744a6f607d90ef9dc6037d3a67e4ee8
Ratnadeep Bhattacharya <bhattacharya.ratnadeep@gmail.com>
2020-11-17 04:06:12 +0000

    adding a routing table struct

---
+++ sw-engine/engine/src/net/routing_table.rs
@@ -0,0 +1,71 @@
+/*
+ * Created on Tue Nov 17 2020:03:16:30
+ * Created by Ratnadeep Bhattacharya
+ */
+
+use super::mac::MacAddr;
+use crossbeam_utils::sync::ShardedLock; // faster reads than RWLock but slower writes
+use std::{collections::HashMap, net::Ipv4Addr, sync::Arc};
+
+/// Here we try to build a routing table that contains a mapping between a MAC and IP of containers running on the server
+/// This table is populated only for response packets
+pub struct RoutingTable {
+	// An alternate approach would be to have a global routing table protected by
+	// a ShardedLock. A single write access to that lock would enable updating both tables
+	// Taking separate, concurrent locks on two tables is more synchronisation overhead
+	mac_table: Arc<ShardedLock<HashMap<MacAddr, Ipv4Addr>>>,
+	ip_table: Arc<ShardedLock<HashMap<Ipv4Addr, MacAddr>>>,
+}
+
+impl RoutingTable {
+	/// Add a mac and ip to the routing table
+	pub fn add(&mut self, mac: MacAddr, ip: Ipv4Addr) {
+		// take the lock for both tables or neither of them
+		if let Ok(mac_table) = self.mac_table.write() {
+			if let Ok(ip_table) = self.ip_table.write() {
+				(*mac_table).insert(mac, ip);
+				(*ip_table).insert(ip, mac);
+			}
+		}
+	}
+
+	/// Check if the MAC is registered for any of the containers
+	pub fn contains_mac(&self, mac: &MacAddr) -> bool {
+		if let Ok(table) = (*self.mac_table).read() {
+			(*table).contains_key(mac)
+		} else {
+			// ISSUES: Need better handling of PANIC here
+			false
+		}
+	}
+
+	/// Check if the IP is registered for any of the containers
+	pub fn contains_ip(&self, ip: &Ipv4Addr) -> bool {
+		if let Ok(table) = (*self.ip_table).read() {
+			(*table).contains_key(ip)
+		} else {
+			// ISSUES: Need better handling of PANIC here
+			false
+		}
+	}
+
+	/// Returns an Option<&MacAddr>
+	pub fn get_mac(&self, ip: &Ipv4Addr) -> Option<&MacAddr> {
+		if let Ok(table) = (*self.ip_table).read() {
+			(*table).get(ip)
+		} else {
+			// ISSUES: Need better handling of PANIC here
+			None
+		}
+	}
+
+	/// Returns an Option<&Ipv4Addr>
+	pub fn get_ip(&self, mac: &MacAddr) -> Option<&Ipv4Addr> {
+		if let Ok(table) = (*self.mac_table).read() {
+			(*table).get(mac)
+		} else {
+			// ISSUES: Need better handling of PANIC here
+			None
+		}
+	}
+}

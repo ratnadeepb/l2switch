6388c8e2be5e77ee2aa3c9f08b328cda105da89d
Ratnadeep Bhattacharya <bhattacharya.ratnadeep@gmail.com>
2020-11-18 14:19:13 +0000

    Moving from std VecDeque to crossbeam ArrayQueue

--- sw-engine/engine/src/runtime/coremap.rs
+++ sw-engine/engine/src/runtime/coremap.rs
@@ -4,28 +4,20 @@
  */
 
 use crate::{
-	debug,
-	dpdk::Mbuf,
-	error, ffi, info,
+	dpdk::Mbuf, info,
 	net::{EtherHdr, FiveTuple, Ipv4Hdr},
-	PortIdMbuf, PortQueue, ETHER_HDR_SZ, FORWARDING_TABLE, IPV4_HDR_SZ, PACKET_READ_SIZE,
-	PORTMAP, PORTS,
+	PortIdMbuf, FORWARDING_TABLE, PORTMAP,
+	PORTS,
 };
-use dpdk_ffi;
-use failure::{Fail, Fallible};
-use futures;
-use std::collections::VecDeque;
+use crossbeam_queue::ArrayQueue;
+use futures::{self, task::LocalSpawnExt};
 
 /// Packet receive function
 /// Receive packets from the NIC and place them in the deque mbufs
-async fn rx_main(
+pub async fn rx_main(
 	mut receiver: futures::channel::oneshot::Receiver<()>,
-	mbufs: &mut VecDeque<PortIdMbuf>,
-) {
-	// let pkts: Vec<Mbuf> = Vec::with_capacity(PACKET_READ_SIZE);
-	// let rx_count: usize;
-	// let cur_lcore = unsafe { dpdk_ffi::_rte_lcore_id() };
-	// let ports: &Vec<PortQueue> = &*(*PORTS);
+	mbufs: &ArrayQueue<PortIdMbuf>,
+) -> () {
 	let ports = PORTS.get();
 
 	loop {
@@ -33,70 +25,76 @@ async fn rx_main(
 			Ok(_) => break, // if we receive anything over the channel then stop receiving
 			Err(futures::channel::oneshot::Canceled) => (),
 		}
-		let mut deque = VecDeque::new();
-		for port in ports {
-			let mbufs = port.receive(); // receive a batch of 32 packets from
+		// let mut deque = VecDeque::new();
+		while !mbufs.is_full() {
+			for port in ports {
+				let recvd = port.receive(); // receive a batch of 32 packets from
 							// push to the end of the VecDeque, pop will happen from the front
-			deque.push_back(PortIdMbuf {
-				portid: port.get_portid(),
-				buf: mbufs,
-			});
+				match mbufs.push(PortIdMbuf {
+					portid: port.get_portid(),
+					buf: recvd,
+				}) {
+					Ok(()) => {}
+					Err(_) => info!("Failed to push pkt"),
+				};
+			}
 		}
-		mbufs.append(&mut deque);
-		// unsafe { MBUFS.append(&mut deque) };
-		// MBUFS.with(|tls| tls.set(deque)); // store the packets locally in the thread
 	}
+	()
 }
 
 /// Consumes the packets placed in the deque by rx_main
 /// Extract the five tuple from each mbuf
 /// Associate the five tuple with a port id
 /// Update the routing table
-async fn process_packets(mbufs: &mut VecDeque<PortIdMbuf>) {
-	// let mut deque: VecDeque<PortIdMbuf> = VecDeque::new();
-	// let deque = unsafe { MBUFS.drain(..).collect::<VecDeque<_>>() };
-	// MBUFS.with(|list| {
-	// 	// the take() consumes the memory leaving the RefCell in default state
-	// 	deque = list.take();
-	// }); // MBUFS is released
-	// the loop consumes the mbufs stored in the VecDeque
-	// iterating over a VecDeque goes front to back
+pub async fn process_packets(mbufs: &ArrayQueue<PortIdMbuf>) {
+	while !mbufs.is_empty() {
+		match mbufs.pop() {
+			Some(elem) => {
+				let pnum = elem.portid;
+				let mbufs = elem.buf;
+				if !mbufs.is_empty() {
+					for mut mbuf in mbufs {
+						let ether_hdr = EtherHdr::from_mbuf(&mut mbuf);
+						let ipv4_hdr = Ipv4Hdr::from_mbuf(&mut mbuf);
+						let five_tuple = FiveTuple::new(ipv4_hdr, ether_hdr);
+						let d_mac = five_tuple.get_d_mac();
+						let d_ip = five_tuple.get_d_ip();
+						// Add five tuple to the portmap
+						let portmap = PORTMAP.get();
+						portmap.insert(pnum, five_tuple);
 
-	let deque = mbufs.drain(..).collect::<VecDeque<_>>();
-	for elem in deque {
-		let pnum = elem.portid;
-		let mbufs = elem.buf;
-		if !mbufs.is_empty() {
-			for mut mbuf in mbufs {
-				let ether_hdr = EtherHdr::from_mbuf(&mut mbuf);
-				let ipv4_hdr = Ipv4Hdr::from_mbuf(&mut mbuf);
-				let five_tuple = FiveTuple::new(ipv4_hdr, ether_hdr);
-				let d_mac = five_tuple.get_d_mac();
-				let d_ip = five_tuple.get_d_ip();
-				// Add five tuple to the portmap
-				let portmap = PORTMAP.get();
-				portmap.insert(pnum, five_tuple);
+						// update forwarding table
+						let forwarding_table = FORWARDING_TABLE.get();
+						forwarding_table.add(d_mac, d_ip);
 
-				// update forwarding table
-				let forwarding_table = FORWARDING_TABLE.get();
-				forwarding_table.add(d_mac, d_ip);
+						// process the packet
+						tx_to_clients(mbuf);
+					}
+				}
 			}
-		}
+			None => 
+				info!("Since we check that the array is non-empty before popping an mbuf, this branch should not have run!")
+		};
 	}
 }
 
 /// Transmit packets to L3 containers through DPDK rings
-async fn tx_main(mut receiver: futures::channel::oneshot::Receiver<()>) {
-	loop {
-		match receiver.try_recv() {
-			Ok(_) => break,
-			Err(futures::channel::oneshot::Canceled) => (),
-		}
-		unimplemented!();
-	}
+/// Consumes the buffer
+fn tx_to_clients(mut mbuf: Mbuf) {
+	unimplemented!();
 }
+
 /// Function for each thread to run
-fn work_horse() {
-	let s = futures::executor::LocalPool::new();
-	let rx = async {};
+fn work_horse(
+	receiver: futures::channel::oneshot::Receiver<()>,
+	mbufs: &'static ArrayQueue<PortIdMbuf>,
+) {
+	let mut executor = futures::executor::LocalPool::new();
+	let spawner = executor.spawner();
+	let rx_fut = rx_main(receiver, mbufs); // get packets
+	let process_pkts_fut = process_packets(mbufs); // process packets
+	let rx_fut_handle = spawner.spawn_local_with_handle(rx_fut).unwrap();
+	spawner.spawn_local(process_pkts_fut).unwrap();
+	executor.run_until(rx_fut_handle); // drop everything the moment the rx_main function returns
 }
